9c0989b6e1e2f7ea0a00ea67f59f2ce5f3502c6a
Jimin0430 <tangbole0430@gmail.com>
2024-11-25 02:39:17 +0900

    feat: 기본 경로 리다이렉트 설정

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,15 @@ const nextConfig: NextConfig = {
     locales: ["en", "ko", "fr"], // 다국어 지원
     defaultLocale: "ko",
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/middle", // 원하는 경로
+        permanent: true, // 영구 리다이렉트 (301 상태)
+      },
+    ];
+  },
 };
 
 export default nextConfig;
---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function Home() {
+  return null; // 아무 내용도 렌더링하지 않음
+}
